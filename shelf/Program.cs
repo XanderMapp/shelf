9599d34e2bf2347e904cd2d1f380673df44ec971
Xander Mapp <xandermapp@gmail.com>
2023-12-22 15:02:48 -0700

    Started work on database integration with SQLite.

--- shelf/Program.cs
+++ shelf/Program.cs
@@ -1,4 +1,5 @@
-﻿namespace Shelf
+﻿using Microsoft.Data.Sqlite;
+namespace Shelf
 {
     internal class Program
     {
@@ -10,59 +11,48 @@
                 char alterType;
                 char[] properMutators = new char[5];
                 bool userHasQuit = false;
-                
+                SqliteConnection dbConn;
+
+
+                dbConn = CreateConnection();
+
                 //Instantiate a new ShelfParser
                 Parser shelfParser = new Parser();
-                
+
                 while (!userHasQuit)
                 {
-                    alterType = GetValidChar("What would you like to alter:" +
-                                                "(A) - Files" +
-                                                "(B) - Folders ");
-                    switch (alterType)
+                    List<File> newDirListFiles;
+                    List<Folder> newDirListFolders;
+                    
+                    alterType = GetValidChar("What would you like to do? " +
+                                             "(A) - New Directory" +
+                                             "(B) - Directories in Database");
+                    switch (char.ToUpper(alterType))
                     {
                         case 'A':
-                            //Creates two new files for storing info into.
-                            File[] filesToMutate = new File[2];
-                            
-                            Console.Write("What is the path to your files? ");
+                            Console.Write("What is the path to the folder you wish to upload? ");
                             path = Console.ReadLine();
-                            if (path != null)
-                            {
-                                MakeFileListFromDirectory(path);
-                            }
-                            else
-                            {
-                                throw new DirectoryNotFoundException();
-                            }
 
-                            break;
-                        case 'B' :
-                            //Creates two new folders for storing info into.
-                            Folder[] foldersToMutate = new Folder[2];
-                            
-                            Console.Write("What is the path to your folders? ");
-                            path = Console.ReadLine();
                             if (path != null)
                             {
-                               foldersToMutate = MakeFolderListFromDirectory(path);
+                                newDirListFiles = MakeFileListFromDirectory(path);
+                                newDirListFolders = MakeFolderListFromDirectory(path);
+                                InsertData(dbConn,newDirListFiles,newDirListFolders);
                             }
-                            else
-                            {
-                                throw new DirectoryNotFoundException();
-                            }
+                            break;
+                        case 'B':
+                            ReadData(dbConn);
                             break;
                     }
-                    
+
                     Console.Write("Enter File Equation (5 operands max!): ");
                     mutators = Console.ReadLine();
                     if (mutators != null)
                     {
                         properMutators = mutators.ToCharArray();
                     }
+
                     shelfParser.Parse(properMutators);
-                    
-                    
                 }
             }
             catch (Exception ex)
@@ -70,6 +60,7 @@
                 Console.WriteLine($"Error: {ex.Message}");
             }
         }
+
         static char GetValidChar(string prompt)
         {
             string userInput;
@@ -83,11 +74,8 @@
                 Console.Write($"{userInput} is not a valid character! {prompt} ");
                 userInput = Console.ReadLine();
             }
+
             return validChar;
-        }
-        static void GetNames()
-        {
-            string file1, file2;
         }
         static List<File> MakeFileListFromDirectory(string path)
         {
@@ -95,41 +83,101 @@
             FileInfo tempFileInfo;
             List<File> files = new List<File>();
             string[] fileNames;
-            
+
             fileNames = Directory.GetFiles(path);
 
             foreach (string aFileName in fileNames)
             {
                 //get file info
                 tempFileInfo = new FileInfo(aFileName);
-                
+
                 //Creation of new file, with info encoded into it.
-                tempFile = new File(tempFileInfo.Name,tempFileInfo.DirectoryName,tempFileInfo.Extension,tempFileInfo.LastWriteTime,tempFileInfo.Length);
-                
+                tempFile = new File(tempFileInfo.Name, tempFileInfo.DirectoryName, tempFileInfo.Extension,
+                    tempFileInfo.LastWriteTime, tempFileInfo.Length);
+
                 files.Add(tempFile);
             }
+
             return files;
         }
+
         static List<Folder> MakeFolderListFromDirectory(string path)
         {
             Folder tempFolder;
             DirectoryInfo tempFolderInfo;
             List<Folder> folders = new List<Folder>();
             string[] folderNames;
-            
+
             folderNames = Directory.GetDirectories(path);
 
             foreach (string aFolderName in folderNames)
             {
-                //get folder info
+                //Get folder info
                 tempFolderInfo = new DirectoryInfo(aFolderName);
-                
+
                 //Creation of new folder, with info encoded into it.
-                tempFolder = new Folder(tempFolderInfo.Name,tempFolderInfo.FullName,tempFolderInfo.Extension,tempFolderInfo.LastWriteTime);
-                
+                tempFolder = new Folder(tempFolderInfo.Name, tempFolderInfo.FullName, tempFolderInfo.Extension,tempFolderInfo.LastWriteTime);
+
                 folders.Add(tempFolder);
             }
             return folders;
+        }
+
+        static SqliteConnection CreateConnection()
+        {
+            SqliteConnection dbConnection;
+            dbConnection = new SqliteConnection("Data Source=shelfdata.sqlite,Version=3,New=True,Compress=True");
+
+            try
+            {
+                dbConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+
+            return dbConnection;
+        }
+
+        static void InsertData(SqliteConnection connection, List<File> filesToWrite, List<Folder> foldersToWrite)
+        {
+            SqliteCommand dbCommand;
+            dbCommand = connection.CreateCommand();
+            string valueString;
+
+            foreach (File aFile in filesToWrite)
+            {
+                valueString = $"({aFile.Path},{aFile.Name},{aFile.Type},{aFile.LastEdited},{aFile.Size}";
+                const string fileQuery = "INSERT INTO FILES(Path,Name,Type,LastEdited,Size) VALUES(valueString)";
+                dbCommand.CommandText = fileQuery;
+                dbCommand.ExecuteNonQuery();
+            }
+
+            foreach (Folder aFolder in foldersToWrite)
+            {
+                valueString = $"({aFolder.Path},{aFolder.Name},{aFolder.Type},{aFolder.LastEdited})";
+                const string folderQuery = "INSERT INTO Folders(Path,Name,Type,LastEdited) VALUES(valueString)";
+                dbCommand.CommandText = folderQuery;
+                dbCommand.ExecuteNonQuery();
+            }
+        }
+        static void ReadData(SqliteConnection connection)
+        {
+            SqliteDataReader dbDatareader;
+            SqliteCommand dbCommand;
+            dbCommand = connection.CreateCommand();
+
+            const string dataQuery = "SELECT * FROM Files UNION SELECT * FROM Folders";
+            dbCommand.CommandText = dataQuery;
+
+            dbDatareader = dbCommand.ExecuteReader();
+            while (dbDatareader.Read())
+            {
+                string myreader = dbDatareader.GetString(0);
+                Console.WriteLine(myreader);
+            }
+            connection.Close();
         }
     }
 }
